b4f14fbb1d80393ddaaf0be6a6be6f50aee326d4
HuongLan123 <hustdream2k5@gmail.com>
2024-11-18 02:21:37 +0700

    Frontend_blog

--- src/components/Pages/Home/Homepage.jsx
+++ src/components/Pages/Home/Homepage.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaPen, FaBook } from 'react-icons/fa'; // Thêm icon
+import { FaUser, FaPaperPlane, FaShare, FaPen, FaLaptop, FaBook, FaBookmark, FaHeart } from 'react-icons/fa'; // Các icon
 import './Homepage.css';
 
 const HomePage = () => {
   return (
     <div className="home-container">
       <div className="home-content">
-        <img src="https://thumb.ac-illust.com/26/2651eb206f542bd53f52d344f2a4b525_t.jpeg" alt="Blog Icon" className="blog-icon" /> {/* Icon Blog */}
         <h1 className="app-title">Create New Blog</h1>
         <p className="app-description">
           A simple and beautiful blogging platform to express your thoughts. Start writing your blog now!
         </p>
         <Link to="/post" className="get-started-btn">
-          <FaPen /> Start Writing
+          Start Writing
         </Link>
+      </div>
+
+      {/* Các icon động bao quanh giao diện */}
+      <div className="icon-wrapper">
+        <FaLaptop className="icon-wrapper1"/>
+        <FaBook className="icon-wrapper2" />
+        <FaBookmark className="icon-wrapper3"/>
+        <FaHeart className="icon-wrapper4"/>
+        <FaPen className="icon-wrapper5"/>
+        <FaUser className="icon-wrapper6"/>
+        <FaPaperPlane className="icon-wrapper7"/>
+        <FaShare className="icon-wrapper8"/>
       </div>
     </div>
   );
